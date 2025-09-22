c04e30459fd7c4502b211ad05e8d6b88d25a6c94
Boris Istigechev <boris245@inbox.ru>
2025-09-22 15:53:20 +0700

    first commit

---
+++ new.js
@@ -0,0 +1,52 @@
+/* Здесь располагаются скрипты */
+
+// переписать функцию initFiltersExpand
+function newInitFiltersExpand() {
+  $("#show_all_filters").click(function () {
+    if (window.innerWidth >= 768) {
+      if ($(this).hasClass("expanded")) {
+        $(this).html("Показать все фильтры");
+      } else {
+        $(this).html("Скрыть фильтры");
+      }
+      $(this).toggleClass("expanded");
+    } else {
+      const filters = $(".screener__filters-row").addClass("filters-in-popup");
+      $("#popup_filters > .filters").append(filters);
+      applyPopupContent($("#popup_filters").html(), 400);
+      $("#popup_filters .screener__filters-row").remove();
+      $(".link-arrow").hide();
+    }
+    $(".screener__filters-row").toggleClass("view-all");
+  });
+
+  let observer = new MutationObserver((mutationRecords) => {
+    if (mutationRecords[0].target.style.display === "none") {
+      $(".screener__filters-row")
+        .removeClass("view-all")
+        .removeClass("filters-in-popup");
+      $(".link-arrow").show();
+      newInitFiltersExpand(); // <- initFiltersExpand
+      $(".screener__filters").prepend($(".screener__filters-row"));
+      observer = null;
+    }
+  });
+
+  // при закрытии попапа
+  const popupOverlay = document.querySelector("#popup_overlay");
+  observer.observe(popupOverlay, {
+    attributeFilter: ["style"],
+  });
+}
+
+// добавить после #popup_warning, скрипт убрать
+document.addEventListener("DOMContentLoaded", () => {
+	const popupWarningElem = document.getElementById("popup_warning");
+	const popupFilters = document.createElement("div");
+	popupFilters.class = "popup-content-prepared"
+	popupFilters.id = "popup_filters"
+	popupFilters.innerHTML = `<div b-w7kossr6ap="" class="popup-title">Фильтры</div>
+      <p b-w7kossr6ap="" class="filters"></p>`
+	document.body.insertBefore(popupFilters, popupWarningElem);
+	newInitFiltersExpand();
+});
