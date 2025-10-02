5116ee4c5a2184d08c9b9d6c8c0f1f7da8d6bb95
Boris Istigechev <boris220-2011@mail.ru>
2025-10-02 22:04:17 +0700

    fix tooltip on scrolling

--- new.js
+++ new.js
@@ -41,46 +41,61 @@ function newInitFiltersExpand() {
 
 // переписать функцию initScreenerSettings
 function newInitScreenerSettings() {
-	if (window.innerWidth <= 768){
-		// заменить на html-вёрстку. Создаётся элемент .screener__mobile-popup, а в ней .screener__mobile-popup-content
-		const popupElement = document.createElement("div");
-		const popupContentElement = document.createElement("div");
-		const closeButton = document.createElement("div");
-		popupElement.classList.add("screener__mobile-popup");
-		popupContentElement.classList.add("screener__mobile-popup-content");
-		closeButton.classList.add("screener__popup-close-button");
-		closeButton.innerText = "X"; // вставьте какую-нибудь картинку сами
-		popupContentElement.appendChild(closeButton);
-		popupElement.appendChild(popupContentElement);
-		$(".screener__btns").append(popupElement);
-		$(".screener__mobile-popup-content").append($(".screener__toggle-columns"));
-	}
-    $('#screener_settings_button').click(function(){
-        $('.screener__toggle-columns').toggleClass('active');
-		if (window.innerWidth <= 768){
-			$('.screener__mobile-popup').toggleClass('active');
-		}
-    });
-    $(document).on('click', function (e) {
-        if ($(e.target).closest('#screener_settings_button').length == 0 && $(e.target).closest('.screener__toggle-columns').length == 0) {
-            $('.screener__toggle-columns').removeClass('active');
-        }
-    });
-	$('.screener__popup-close-button').click(function(){
-		console.log("closed");
-		$('.screener__mobile-popup').removeClass('active');
-	})
+  if (window.innerWidth <= 768) {
+    // заменить на html-вёрстку. Создаётся элемент .screener__mobile-popup, а в ней .screener__mobile-popup-content
+    const popupElement = document.createElement("div");
+    const popupContentElement = document.createElement("div");
+    const closeButton = document.createElement("div");
+    popupElement.classList.add("screener__mobile-popup");
+    popupContentElement.classList.add("screener__mobile-popup-content");
+    closeButton.classList.add("screener__popup-close-button");
+    closeButton.innerText = "X"; // вставьте какую-нибудь картинку сами
+    popupContentElement.appendChild(closeButton);
+    popupElement.appendChild(popupContentElement);
+    $(".screener__btns").append(popupElement);
+    $(".screener__mobile-popup-content").append($(".screener__toggle-columns"));
+  }
+  $("#screener_settings_button").click(function () {
+    $(".screener__toggle-columns").toggleClass("active");
+    if (window.innerWidth <= 768) {
+      $(".screener__mobile-popup").toggleClass("active");
+    }
+  });
+  $(document).on("click", function (e) {
+    if (
+      $(e.target).closest("#screener_settings_button").length == 0 &&
+      $(e.target).closest(".screener__toggle-columns").length == 0
+    ) {
+      $(".screener__toggle-columns").removeClass("active");
+    }
+  });
+  $(".screener__popup-close-button").click(function () {
+    $(".screener__mobile-popup").removeClass("active");
+  });
 }
 
 // добавить после #popup_warning, скрипт убрать
 document.addEventListener("DOMContentLoaded", () => {
-	const popupWarningElem = document.getElementById("popup_warning");
-	const popupFilters = document.createElement("div");
-	popupFilters.classList.add("popup-content-prepared")
-	popupFilters.id = "popup_filters"
-	popupFilters.innerHTML = `<div b-w7kossr6ap="" class="popup-title">Фильтры</div>
-      <p b-w7kossr6ap="" class="filters"></p>`
-	document.body.insertBefore(popupFilters, popupWarningElem);
-	newInitFiltersExpand();
-	newInitScreenerSettings();
+  const popupWarningElem = document.getElementById("popup_warning");
+  const popupFilters = document.createElement("div");
+  popupFilters.classList.add("popup-content-prepared");
+  popupFilters.id = "popup_filters";
+  popupFilters.innerHTML = `<div b-w7kossr6ap="" class="popup-title">Фильтры</div>
+      <p b-w7kossr6ap="" class="filters"></p>`;
+  document.body.insertBefore(popupFilters, popupWarningElem);
+  newInitFiltersExpand();
+  newInitScreenerSettings();
+});
+
+let scrollTimer = null;
+
+/* Закрытие тултипа при скроллинге */
+document.addEventListener("scroll", () => {
+  if (!scrollTimer) {
+    $(".tooltip-content").css("visibility", "hidden");
+
+    scrollTimer = setTimeout(() => {
+      scrollTimer = null;
+    }, 200);
+  }
 });
